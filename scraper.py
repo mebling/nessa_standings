9bc5ab53df74017510432694cea22892441ab997
Maritza EBLING <maritza@mylestone.com>
2018-10-10 11:45:10 -0400

    some cleanup

--- scraper.py
+++ scraper.py
@@ -8,18 +8,32 @@ URL = BASE_URL + "standings.asp"
 YEARS = ["_09", "_10", "_11", "_12", "_13", "_14", "_15", "_16", "_17", ""]
 
 
-def scrape(url):
-    page = requests.get(url)
-    doc = lh.fromstring(page.content)
-    links = doc.xpath('//tr//a')[2:]
-    for link in links:
-        school_name = link.text_content()
-        results_link = BASE_URL + link.attrib['href']
+class Scraper:
+    def __init__(self, url):
+        self.url = url
+
+    @property
+    def _school_links(self):
+        page = requests.get(self.url)
+        doc = lh.fromstring(page.content)
+        return doc.xpath('//tr//a')[2:]
+
+    def scrape(self):
+        for link in self._school_links:
+            SchoolScraper(link).scrape()
+
+class SchoolScraper:
+    def __init__(self, link):
+        self.school_name = link.text_content()
+        self.school_url = link.attrib['href']
+
+    def scrape(self):
+        pass
 
 
 def scrape_all():
     for year in YEARS:
-        scrape(URL + year)
+        Scraper(URL + year).scrape()
 
 
 if __name__ == '__main__':

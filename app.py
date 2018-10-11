83eeca78c23daeab0894eaf4fe1f2840796fdc6b
Maritza EBLING <maritza@mylestone.com>
2018-10-11 16:52:16 -0400

    add some more code

---
+++ app.py
@@ -0,0 +1,25 @@
+import os
+from flask import Flask, flash, request, redirect, url_for, render_template
+from flask_cors import CORS
+from flask import jsonify
+import datetime
+from ratings import chart_data
+from models import School
+
+
+app = Flask(__name__)
+CORS(app)
+
+
+@app.route('/', methods=['GET'])
+def index():
+    data, tooltip_data = chart_data()
+    return render_template("chart.html", highchart_json=data[0:1], tooltip_json=tooltip_data)
+
+
+@app.route("/schools/<school_id>", methods=["GET"])
+def schools(school_id):
+    school = School.get(id=school_id)
+    data, tooltip_data = chart_data()
+    data = [d for d in data if d['name'] == school.name]
+    return render_template("chart.html", highchart_json=data, tooltip_json=tooltip_data)
--- ratings.py
+++ ratings.py
@@ -3,6 +3,9 @@ from models import School, Race, db
 from collections import defaultdict
 from tqdm import tqdm
 from functools import lru_cache
+import datetime
+from copy import copy
+
 
 def get_rating(team_name, date):
     return glicko_scores(date)[team_name].rating.mu
@@ -21,3 +24,28 @@ def glicko_scores(date):
         series.extend([[LOSS, opponent_rating.rating]] * race.opponent_score)
         scores[school.name].rate(series)
     return scores
+
+
+def chart_data():
+    chart_data = defaultdict(list)
+    tooltip_data = defaultdict(dict)
+
+    scores = defaultdict(Glicko)
+    race_query = Race.select(Race.school_id, Race.opponent_score, Race.school_score, Race.opponent_id, Race.date).order_by(Race.date)
+    for race in tqdm(race_query):
+        school = race.school_id
+        opponent = race.opponent_id
+        opponent_rating = scores[opponent.name]
+        series = [[WIN, opponent_rating.rating]] * race.school_score
+        series.extend([[LOSS, opponent_rating.rating]] * race.opponent_score)
+        previous_rating = copy(scores[school.name])
+        scores[school.name].rate(series)
+        new_rating = scores[school.name].rating.mu
+        chart_data[school.name].append([race.date, scores[school.name].rating.mu])
+        change = scores[school.name].rating.mu - previous_rating.rating.mu
+        change = "+{}".format(str(round(change, 2))) if change >= 0 else str(round(change, 2))
+        tooltip_data[school.name][race.date] = "{}\n -> {}-{} ({})".format(opponent.name, race.school_score, race.opponent_score, change)
+    formatted = []
+    for key, values in chart_data.items():
+        formatted.append({ 'name': key, 'data': values })
+    return formatted, tooltip_data

1023a99ec73271a8daaa06a36ce9f83a454b4a51
Maritza EBLING <maritza@mylestone.com>
2018-10-12 18:41:25 -0400

    fix the query

--- app.py
+++ app.py
@@ -27,7 +27,7 @@ def index():
 
 @app.route("/schools/<school_id>", methods=["GET"])
 def schools(school_id):
-    school = School.filter_by(id=school_id).first()
+    school = School.query.filter_by(id=school_id).first()
     data, tooltip_data = chart_data()
     data = [d for d in data if d['name'] == school.name]
     return render_template("chart.html", highchart_json=data, tooltip_json=tooltip_data)

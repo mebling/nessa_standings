3332650118dd239ab606bcb1d336d171357c750d
Maritza EBLING <maritza@mylestone.com>
2018-10-16 07:08:11 -0400

    fix the formula

--- glicko_competitor.py
+++ glicko_competitor.py
@@ -20,7 +20,7 @@ class GlickoCompetitor(BaseCompetitor):
         }
 
     def transform_rd(self, min_rd):
-        self.rating_period_rd = min([min_rd, math.sqrt(self.rd ** 2 + self._c ** 2)])
+        self.rd = min([min_rd, math.sqrt(self.rd ** 2 + self._c ** 2)])
 
     @property
     def _g(self):
@@ -30,32 +30,21 @@ class GlickoCompetitor(BaseCompetitor):
         exponent = (-1 * competitor._g * (self.rating - competitor.rating)) / 400.
         return 1 / (1 + 10 ** exponent)
 
-    def raced(self, races, min_rd=350):
-        self.transform_rd(min_rd)
+    def raced(self, races):
         if len(races) == 0:
             return
-        d2_sum = 0
+        d_square_inv = 0
         difference = 0
         for competitor, outcome in races:
             impact = competitor._g
             expected_score = self.expected_score(competitor)
-            if outcome:
-                actual_score = 1.
-            elif outcome == None:
-                actual_score = .5
-            else:
-                actual_score = 0
+            actual_score = 1. if outcome else (.5 if outcome == None else 0.)
             difference += impact * (actual_score - expected_score)
-            d2_sum += expected_score * (1 - expected_score) * (impact ** 2)
-        if d2_sum == 0:
-            return
-        else:
-            d2 = 1/((self._q ** 2) * d2_sum)
-            denom = self.rd ** -2 + 1/d2
+            d_square_inv += (expected_score * (1 - expected_score) * (self._q ** 2) * (impact ** 2))
 
-            # assign new rating and rd
-            self.rating_period_rating = self.rating + ((self._q / denom) * difference)
-            self.rating_period_pd = math.sqrt(1/denom)
+        denom = self.rd ** -2 + d_square_inv
+        self.rating_period_rating = self.rating + self._q / denom * difference
+        self.rating_period_rd = math.sqrt(1. / denom)
 
     def update(self):
         self.rating = self.rating_period_rating
